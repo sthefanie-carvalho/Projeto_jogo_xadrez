1a452f2e81361ecd02dcf4980ca958e993de0040
Sthefanie Carvalho <sthefanie.carvalho@outlook.com>
2020-10-08 16:35:16 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -11,15 +11,22 @@ namespace xadrez_console
 
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
 
-                Tela.imprimirTabuleiro(tab);
+                    partida.executarMovimento(origem, destino);
+                }
+
             }
             catch (TabuleiroException e)
             {
--- xadrez_console/tabuleiro/Peca.cs
+++ xadrez_console/tabuleiro/Peca.cs
@@ -18,6 +18,10 @@ namespace tabuleiro
             this.qteMovimentos = 0;
         }
 
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
 
 
     }
