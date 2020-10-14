4959258d9d2b75899dd7dc470c08935cfc820e63
Sthefanie Carvalho <sthefanie.carvalho@outlook.com>
2020-10-14 12:04:29 -0300

    Jogada especial promocao

--- xadrez_console/xadrez/PartidaDeXadrez.cs
+++ xadrez_console/xadrez/PartidaDeXadrez.cs
@@ -75,6 +75,18 @@ namespace xadrez {
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce não pode se colocar em xeque!");
             }
+            Peca p = tab.peca(destino);
+            //#jogadaespecial promocao
+            if (p is Peao) {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             }
@@ -88,7 +100,6 @@ namespace xadrez {
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)){
                 vulneravelEnPasssant = p;
